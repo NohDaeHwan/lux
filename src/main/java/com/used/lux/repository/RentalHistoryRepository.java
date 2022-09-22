a91d97e464ad872f98f526e1514ecfa8f837c772
NohDaeHwan <ndh5953@naver.com>
2022-09-22 12:31:46 +0900

    #3 - 렌탈 서비스 제외로 인한 도메인 삭제

--- src/main/java/com/used/lux/repository/RentalHistoryRepository.java
+++ src/main/java/com/used/lux/repository/RentalHistoryRepository.java
@@ -1,6 +1,5 @@
 package com.used.lux.repository;
 
-import com.used.lux.domain.RentalHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentalHistoryRepository extends JpaRepository<RentalHistory,Long> {
