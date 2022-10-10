a176669a2c6721974a70b889144a45c6b96e862b
NohDaeHwan <ndh5953@naver.com>
2022-10-10 11:43:01 +0900

    JPA Config
    Security 설정으로 Auditing 구현

--- src/main/java/com/used/lux/config/JpaConfig.java
+++ src/main/java/com/used/lux/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.used.lux.config;
 
+import com.used.lux.dto.security.Principal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,7 +17,12 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware() {
-        return () -> Optional.of("eoghks");
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(Principal.class::cast)
+                .map(Principal::getUsername);
     }
 
 }
