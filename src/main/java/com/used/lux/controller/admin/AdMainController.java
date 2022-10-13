25474d909f6f990d54126c8720929d0aebcde569
gunahyeon <rnskqkd@naver.com>
2022-10-13 13:26:59 +0900

    admin 디자인

--- src/main/java/com/used/lux/controller/admin/AdMainController.java
+++ src/main/java/com/used/lux/controller/admin/AdMainController.java
@@ -2,6 +2,7 @@ package com.used.lux.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdMainController {
 
     @GetMapping
-    public String index() {
-        return "/admin/index";
+    public String test(ModelMap mm) {
+        mm.addAttribute("hello","ㅇㅏㄴ녕");
+        return "/admin/test";
     }
 
 }
