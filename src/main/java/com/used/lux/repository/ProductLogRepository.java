dddb51f518bd82d5f192705556c4dd5b35170d76
NohDaeHwan <ndh5953@naver.com>
2022-10-14 23:03:03 +0900

    #3 - 경매 및 상품 레포지토리
    브랜드, 카테고리, 상태

--- src/main/java/com/used/lux/repository/ProductLogRepository.java
+++ src/main/java/com/used/lux/repository/ProductLogRepository.java
@@ -3,5 +3,8 @@ package com.used.lux.repository;
 import com.used.lux.domain.ProductLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductLogRepository extends JpaRepository<ProductLog, Long> {
+    List<ProductLog> findByProductId(Long productId);
 }
---
+++ src/main/java/com/used/lux/repository/StateRepository.java
@@ -0,0 +1,7 @@
+package com.used.lux.repository;
+
+import com.used.lux.domain.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+}
