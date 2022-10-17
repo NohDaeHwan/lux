a9a83c350cc0c923f324d8930a1411762a9e3d3b
NohDaeHwan <ndh5953@naver.com>
2022-10-17 15:11:54 +0900

    #3 - 레포지토리 추가

---
+++ src/main/java/com/used/lux/repository/CategoryMRepository.java
@@ -0,0 +1,7 @@
+package com.used.lux.repository;
+
+import com.used.lux.domain.CategoryM;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryMRepository extends JpaRepository<CategoryM, Long> {
+}
--- src/main/java/com/used/lux/request/AppraisalCommentRequest.java
+++ src/main/java/com/used/lux/request/AppraisalCommentRequest.java
@@ -1,11 +1,20 @@
 package com.used.lux.request;
 
 public record AppraisalCommentRequest(
+        String appraisalName,
+        String appraiseBrand,
+        String appraisalContent,
+        String appraisalGender,
+        String appraisalColor,
+        String appraisalSize,
         String appraisalGrade,
-        String appraisalComment,
-        int appraisalPrice
+        int appraisalPrice,
+        String appraisalComment
 )  {
-    public static AppraisalCommentRequest of(String appraisalGrade, String appraisalComment, int appraisalPrice) {
-        return new AppraisalCommentRequest(appraisalGrade, appraisalComment, appraisalPrice);
+    public static AppraisalCommentRequest of(String appraisalName, String appraiseBrand, String appraisalContent,
+                                   String appraisalGender, String appraisalColor, String appraisalSize, String appraisalGrade,
+                                   int appraisalPrice, String appraisalComment) {
+        return new AppraisalCommentRequest(appraisalName, appraiseBrand, appraisalContent, appraisalGender, appraisalColor,
+                appraisalSize, appraisalGrade, appraisalPrice, appraisalComment);
     }
 }
