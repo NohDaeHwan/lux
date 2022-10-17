3ff2e187a61172bc628f39b03ecee8436bfda368
NohDaeHwan <ndh5953@naver.com>
2022-10-17 15:11:20 +0900

    #3 - service 추가

---
+++ src/main/java/com/used/lux/service/UserAccountLogService.java
@@ -0,0 +1,12 @@
+package com.used.lux.service;
+
+import com.used.lux.repository.UserAccountLogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserAccountLogService {
+    //UserAccountLog DB의 서비스 클래스입니다. 해당 DB에 연결하려면 repository와 service 영역에 추가적인 작성이 필요합니다.
+    private final UserAccountLogRepository userAccountLogRepository;
+}
