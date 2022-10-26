f2994a891f0a5e0f85e9673c531f0df39972b502
awdsc <ku02050@gmail.com>
2022-10-26 18:32:59 +0900

    Update stateDto.java

--- src/main/java/com/used/lux/dto/stateDto.java
+++ src/main/java/com/used/lux/dto/stateDto.java
@@ -1,8 +1,22 @@
 package com.used.lux.dto;
 
-public record stateDto(
+import com.used.lux.domain.State;
+
+public record StateDto(
         Long id,
-        String state_name,
-        String state_step
-) {
+        String stateName,
+        String stateStep
+)  {
+    public static StateDto of(Long id, String stateName, String stateStep) {
+        return new StateDto(id, stateName, stateStep);
+    }
+
+    public static StateDto from(State entity) {
+        return new StateDto(
+                entity.getId(),
+                entity.getStateName(),
+                entity.getStateStep()
+        );
+    }
+
 }
