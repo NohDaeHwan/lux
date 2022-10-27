6a5fa06f4fc5024eedb16a1c683c2d1184b0ff87
NohDaeHwan <ndh5953@naver.com>
2022-10-27 09:10:01 +0900

    #3 - Admin appraise 조회 및 페이징 구현

---
+++ src/main/java/com/used/lux/repository/querydsl/AppraisalRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.used.lux.repository.querydsl;
+
+import com.used.lux.domain.Appraisal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AppraisalRepositoryCustom {
+
+    Page<Appraisal> searchAppraise(String appraisalState, String appraisalBrand, String appraisalGender,
+                                  String appraisalSize, String appraisalGrade, String appraisalDate,
+                                  String query, Pageable pageable);
+
+}
