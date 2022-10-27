1927600d49533eb8582db5097566bb1565433980
NohDaeHwan <ndh5953@naver.com>
2022-10-27 17:55:45 +0900

    #3 - 파일 핸들러 구현

--- src/main/java/com/used/lux/component/FileHandler.java
+++ src/main/java/com/used/lux/component/FileHandler.java
@@ -1,9 +1,8 @@
 package com.used.lux.component;
 
-import com.used.lux.domain.Appraisal;
 import com.used.lux.domain.Image;
-import com.used.lux.domain.UserAccount;
-import com.used.lux.dto.ProductDto;
+import com.used.lux.domain.Product;
+import com.used.lux.request.ProductCreateRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
@@ -18,12 +17,11 @@ import java.util.List;
 @Component
 public class FileHandler {
 
-	public List<Image> parseFileInfo(ProductDto dto) throws Exception {
-		// 반환할 파일 리스트
+	public List<Image> parseFileInfo(ProductCreateRequest productCreateRequest, Product product) throws Exception {
 		List<Image> imageList =  new ArrayList<>();
 
 		// 전달되어 온 파일이 존재할 경우
-        if(!CollectionUtils.isEmpty(dto.imageDtos())) {
+        if(!CollectionUtils.isEmpty(productCreateRequest.images())) {
             // 파일명을 업로드 한 날짜로 변환하여 저장
             LocalDateTime now = LocalDateTime.now();
             DateTimeFormatter dateTimeFormatter =
@@ -33,12 +31,11 @@ public class FileHandler {
             // 프로젝트 디렉터리 내의 저장을 위한 절대 경로 설정
             // 경로 구분자 File.separator 사용
             String absolutePath = new File("").getAbsolutePath() + File.separator + File.separator;
-            System.out.println(absolutePath);
 
             // 파일을 저장할 세부 경로 지정
             String path = absolutePath + "src" + File.separator + "main" + File.separator + "resources" + File.separator +
             		"static" + File.separator + "assets" + File.separator + "img" + File.separator +
-            		"appraisal_img" + File.separator + current_date;
+            		"product_img" + File.separator + current_date;
             File file = new File(path);
             System.out.println(path);
             System.out.println(file.exists());
@@ -54,50 +51,49 @@ public class FileHandler {
             }
 
             // 다중 파일 처리
-//            for(MultipartFile multipartFile : dto.imageDtos() ) {
-//
-//                    // 파일의 확장자 추출
-//                    String originalFileExtension;
-//                    String contentType = multipartFile.getContentType();
-//
-//                    // 확장자명이 존재하지 않을 경우 처리 x
-//                    if(ObjectUtils.isEmpty(contentType)) {
-//                        break;
-//                    }
-//                    else {  // 확장자가 jpeg, png인 파일들만 받아서 처리
-//                        if(contentType.contains("image/jpeg"))
-//                            originalFileExtension = ".jpg";
-//                        else if(contentType.contains("image/png"))
-//                            originalFileExtension = ".png";
-//                        else  // 다른 확장자일 경우 처리 x
-//                            break;
-//                    }
-//
-//                    // 파일명 중복 피하고자 나노초까지 얻어와 지정
-//                    String new_file_name = System.nanoTime() + originalFileExtension;
-//
-//                    // AppraisalImage 엔티티 생성
-//                    Image image = Image.of(
-//                            appraisal,
-//                            userAccount,
-//                            multipartFile.getOriginalFilename(),
-//                            "/static/assets/img/appraisal_img/" + current_date + "/" + new_file_name,
-//                            multipartFile.getSize()
-//                    );
-//
-//                    // 생성 후 리스트에 추가
-//                    imageList.add(image);
-//
-//                    // 업로드 한 파일 데이터를 지정한 파일에 저장
-//                    file = new File(absolutePath + "src" + File.separator + "main" + File.separator + "resources" + File.separator +
-//                    		"static" + File.separator + "assets" + File.separator + "img" + File.separator +
-//                    		"appraisal_img" + File.separator + current_date + File.separator + new_file_name);
-//                    multipartFile.transferTo(file);
-//
-//                    // 파일 권한 설정(쓰기, 읽기)
-//                    file.setWritable(true);
-//                    file.setReadable(true);
-//            }
+            for(MultipartFile multipartFile : productCreateRequest.images()) {
+
+                    // 파일의 확장자 추출
+                    String originalFileExtension;
+                    String contentType = multipartFile.getContentType();
+
+                    // 확장자명이 존재하지 않을 경우 처리 x
+                    if(ObjectUtils.isEmpty(contentType)) {
+                        break;
+                    }
+                    else {  // 확장자가 jpeg, png인 파일들만 받아서 처리
+                        if(contentType.contains("image/jpeg"))
+                            originalFileExtension = ".jpg";
+                        else if(contentType.contains("image/png"))
+                            originalFileExtension = ".png";
+                        else  // 다른 확장자일 경우 처리 x
+                            break;
+                    }
+
+                    // 파일명 중복 피하고자 나노초까지 얻어와 지정
+                    String new_file_name = System.nanoTime() + originalFileExtension;
+
+                    // AppraisalImage 엔티티 생성
+                    Image image = Image.of(
+                            product,
+                            multipartFile.getOriginalFilename(),
+                            "/static/assets/img/product_img/" + current_date + "/" + new_file_name,
+                            multipartFile.getSize()
+                    );
+
+                    // 생성 후 리스트에 추가
+                    imageList.add(image);
+
+                    // 업로드 한 파일 데이터를 지정한 파일에 저장
+                    file = new File(absolutePath + "src" + File.separator + "main" + File.separator + "resources" + File.separator +
+                    		"static" + File.separator + "assets" + File.separator + "img" + File.separator +
+                    		"product_img" + File.separator + current_date + File.separator + new_file_name);
+                    multipartFile.transferTo(file);
+
+                    // 파일 권한 설정(쓰기, 읽기)
+                    file.setWritable(true);
+                    file.setReadable(true);
+            }
         }
         return imageList;
 	}
