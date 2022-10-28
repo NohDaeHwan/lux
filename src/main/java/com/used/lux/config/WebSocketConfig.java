05ed8b7c98b8d658841970482e20b46c7852f5d6
NohDaeHwan <ndh5953@naver.com>
2022-10-28 23:00:35 +0900

    #3 - 프론트 View

---
+++ src/main/java/com/used/lux/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.used.lux.config;
+
+import com.used.lux.service.socket.WebSocketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketService webSocketService;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+        registry.addHandler(webSocketService, "ws/auction/21/bidder").setAllowedOrigins("*");
+    }
+
+}
