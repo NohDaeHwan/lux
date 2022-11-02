d424418689cdf04f33ff995b89f704d439044643
NohDaeHwan <ndh5953@naver.com>
2022-11-02 21:10:13 +0900

    #3 - 도메인 수정 데이터 변경

--- src/main/java/com/used/lux/dto/UserAccountLogDto.java
+++ src/main/java/com/used/lux/dto/UserAccountLogDto.java
@@ -7,26 +7,31 @@ import java.time.LocalDateTime;
 public record UserAccountLogDto(
         Long id,
         String userEmail,
-        int point,
         String gradeName,
         int discount,
+        int point,
+        String usageDetail,
+        String saleNumber,
         LocalDateTime createdAt,
         String createdBy,
         LocalDateTime modifiedAt,
         String modifiedBy
 ) {
-    public static UserAccountLogDto of(Long id, String userEmail, int point, String gradeName, int discount,
-                             LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountLogDto(id, userEmail, point, gradeName, discount, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static UserAccountLogDto of(Long id, String userEmail, String gradeName, int discount, int point,
+                                       String usageDetail, String saleNumber, LocalDateTime createdAt,
+                                       String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountLogDto(id, userEmail, gradeName, discount, point, usageDetail, saleNumber, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     public static UserAccountLogDto from(UserAccountLog entity) {
         return new UserAccountLogDto(
                 entity.getId(),
                 entity.getUserEmail(),
-                entity.getPoint(),
                 entity.getUserGrade().getGradeName(),
                 entity.getUserGrade().getDiscount(),
+                entity.getPoint(),
+                entity.getUsageDetail(),
+                entity.getSaleNumber(),
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),
                 entity.getModifiedAt(),
