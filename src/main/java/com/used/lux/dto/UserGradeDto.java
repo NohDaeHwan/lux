fddf9354107a04f41a76646432a9d883e68a7993
NohDaeHwan <ndh5953@naver.com>
2022-11-02 21:09:08 +0900

    #3 - 등급 도메인 수정

--- src/main/java/com/used/lux/dto/UserGradeDto.java
+++ src/main/java/com/used/lux/dto/UserGradeDto.java
@@ -4,18 +4,22 @@ import com.used.lux.domain.UserGrade;
 
 public record UserGradeDto(
         Long id,
+        int gradeStep,
         String gradeName,
-        int discount
+        int discount,
+        int rankUp
 ) {
-    public static UserGradeDto of(Long id, String gradeName, int discount) {
-        return new UserGradeDto(id, gradeName, discount);
+    public static UserGradeDto of(Long id, int gradeStep, String gradeName, int discount, int rankUp) {
+        return new UserGradeDto(id, gradeStep, gradeName, discount, rankUp);
     }
 
     public static UserGradeDto from(UserGrade entity) {
         return new UserGradeDto(
                 entity.getId(),
+                entity.getGradeStep(),
                 entity.getGradeName(),
-                entity.getDiscount()
+                entity.getDiscount(),
+                entity.getRankUp()
         );
     }
 
