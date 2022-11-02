f4c75fdd186b94f7fdfc1225d57312947d3b9b87
NohDaeHwan <ndh5953@naver.com>
2022-11-02 21:14:42 +0900

    #3 - 레포지토리

--- src/main/java/com/used/lux/request/GradeCreateRequest.java
+++ src/main/java/com/used/lux/request/GradeCreateRequest.java
@@ -2,10 +2,12 @@ package com.used.lux.request;
 
 public record GradeCreateRequest(
         Long id,
+        int gradeStep,
         String gradeName,
-        int discount
+        int discount,
+        int rankUp
 ) {
-    public static GradeCreateRequest of(Long id, String gradeName, int discount) {
-        return new GradeCreateRequest(id, gradeName, discount);
+    public static GradeCreateRequest of(Long id, int gradeStep, String gradeName, int discount, int rankUp) {
+        return new GradeCreateRequest(id, gradeStep, gradeName, discount, rankUp);
     }
 }
