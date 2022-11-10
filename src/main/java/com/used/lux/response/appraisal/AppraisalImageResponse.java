cc021122d036711a703909bb8395c5adb380dbbe
NohDaeHwan <ndh5953@naver.com>
2022-11-10 20:48:52 +0900

    #3 - 검수 리스트, 상세, 등록

---
+++ src/main/java/com/used/lux/response/appraisal/AppraisalImageResponse.java
@@ -0,0 +1,36 @@
+package com.used.lux.response.appraisal;
+
+import com.used.lux.dto.AppraisalImageDto;
+
+import java.time.LocalDateTime;
+
+public record AppraisalImageResponse(
+        Long AppraisalRequestId,
+        String origFileName,
+        String filePath,
+        Long fileSize,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+)  {
+
+    public static AppraisalImageResponse of(Long AppraisalRequestId, String origFileName, String filePath, Long fileSize,
+                                            LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new AppraisalImageResponse(AppraisalRequestId, origFileName, filePath, fileSize, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static AppraisalImageResponse from(AppraisalImageDto dto) {
+        return new AppraisalImageResponse(
+                dto.AppraisalRequestId(),
+                dto.origFileName(),
+                dto.filePath(),
+                dto.fileSize(),
+                dto.createdAt(),
+                dto.createdBy(),
+                dto.modifiedAt(),
+                dto.modifiedBy()
+        );
+    }
+
+}
