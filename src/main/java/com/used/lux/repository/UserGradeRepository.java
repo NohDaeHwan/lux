793bf2aca6b225b363d62b2b1b34ea7b786749a7
gunahyeon <rnskqkd@naver.com>
2022-11-10 20:49:43 +0900

    마이페이지 마지막 등급 확보

--- src/main/java/com/used/lux/repository/UserGradeRepository.java
+++ src/main/java/com/used/lux/repository/UserGradeRepository.java
@@ -2,7 +2,12 @@ package com.used.lux.repository;
 
 import com.used.lux.domain.UserGrade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserGradeRepository extends JpaRepository<UserGrade, Long> {
+
     UserGrade findByGradeStep(int gradeStep);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM user_grade where grade_step = (select max(grade_step) from user_grade)")
+    UserGrade findByLastStep();
 }
