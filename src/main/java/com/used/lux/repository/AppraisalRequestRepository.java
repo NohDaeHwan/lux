6990da156ca9e6c08844932eef4ba39070dfe9ca
NohDaeHwan <ndh5953@naver.com>
2022-11-11 19:39:19 +0900

    #3 - 충돌 수정

--- src/main/java/com/used/lux/repository/AppraisalRequestRepository.java
+++ src/main/java/com/used/lux/repository/AppraisalRequestRepository.java
@@ -1,14 +1,17 @@
 package com.used.lux.repository;
 
-import com.used.lux.domain.Appraisal;
 import com.used.lux.domain.AppraisalRequest;
-import com.used.lux.repository.querydsl.AppraisalRepositoryCustom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface AppraisalRequestRepository extends JpaRepository<AppraisalRequest, Long> {
 
-    List<AppraisalRequest> findByUserAccountId(Long id);
+    Page<AppraisalRequest> findByUserAccountId(Long id, Pageable pageable);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM appraisal_request WHERE user_account_id = :id")
+    List<AppraisalRequest> findByUserAccountId2(Long id);
 }
