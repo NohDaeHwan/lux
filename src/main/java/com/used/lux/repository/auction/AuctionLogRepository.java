f77d6db3872a5ad436585d7eb87592d3161da600
kyungwon song <ruddnjs1289@gmail.com>
2022-11-17 22:00:35 +0900

    마이페이지 내역 수정

--- src/main/java/com/used/lux/repository/auction/AuctionLogRepository.java
+++ src/main/java/com/used/lux/repository/auction/AuctionLogRepository.java
@@ -1,15 +1,25 @@
 package com.used.lux.repository.auction;
 
+import com.querydsl.core.group.GroupBy;
 import com.used.lux.domain.auction.AuctionLog;
+import com.used.lux.dto.user.auction.AuctionLogDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface AuctionLogRepository extends JpaRepository<AuctionLog, Long> {
-    List<AuctionLog> findByBidder(String userName);
+
+    @Query(nativeQuery = true, value ="SELECT * FROM auction_log WHERE bidder = :userName")
+    List<AuctionLog> findByBidderList(String userName);
 
     List<AuctionLog> findByProductId(Long productId);
 
     List<AuctionLog> findByAuctionId(Long auctionId);
+
+    //마이페이지 경매 내역 조회
+    Page<AuctionLog> findByBidder(String bidder, Pageable pageable);
 }
--- src/main/java/com/used/lux/service/user/auction/AuctionLogService.java
+++ src/main/java/com/used/lux/service/user/auction/AuctionLogService.java
@@ -3,7 +3,10 @@ package com.used.lux.service.user.auction;
 import com.used.lux.dto.user.auction.AuctionLogDto;
 import com.used.lux.repository.auction.AuctionLogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,5 +20,10 @@ public class AuctionLogService {
     public List<AuctionLogDto> auctionLogList(Long auctionId) {
         return auctionLogRepository.findByAuctionId(auctionId).stream()
                 .map(AuctionLogDto::from).collect(Collectors.toUnmodifiableList());
+    }
+
+
+    public  Page<AuctionLogDto> searchAuctionLog(String bidder,Pageable pageable){
+        return auctionLogRepository.findByBidder(bidder,pageable).map(AuctionLogDto::from);
     }
 }
